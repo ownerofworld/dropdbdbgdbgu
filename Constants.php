0df9c9e27ea6e27ca4481a2e085de7cf260a2201
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2019-11-06 15:03:22 +0530

    Update Constants.php

--- Constants.php
+++ Constants.php
@@ -2,4 +2,4 @@
 
 const BOT_SESSION = 'teleupload.madeline';
 const TMP_DOWNLOADS = '.' . DIRECTORY_SEPARATOR . 'tmp_downloads';
-const SETTINGS = ['app_info' => ['api_id' => 'ENTER_YOUR', 'api_hash' => 'ENTER_YOUR_HASH']];
+const SETTINGS = ['app_info' => ['api_id' => '1162693', 'api_hash' => 'ebdb74f078e513cc298ae9bedb2ae9f3']];
