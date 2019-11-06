15012cecb7b271df96376bdd5912cd3a6edc8243
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2019-11-06 15:02:22 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export BOT_TOKEN='YOUR_TOKEN_HERE';
+export BOT_TOKEN='998013377:AAEOuQ6VHZO-qgs4DLH9MmazFRWcbRAAKrc';
 export DB_ID='YOUR_ID_HERE';
 export DB_SECRET='YOUR_SECRET_HERE';
 export DB_TOKEN='YOUR_TOKEN_HERE';
